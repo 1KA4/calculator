aec5fa9c0fae1c15ddc673f27c68871b1c1b2697
Ivan Kachur <46796379+1KA4@users.noreply.github.com>
2020-11-04 23:58:41 +0100

    Creating...

---
+++ js/script.js
@@ -0,0 +1,53 @@
+"use strict"; // Created by 1KA4
+//document.ondragstart = noselect; // запрет на перетаскивание 
+//document.onselectstart = noselect; // запрет на выделение элементов страницы 
+//document.oncontextmenu = noselect; // запрет на выведение контекстного меню 
+//function noselect() {return false;} 
+
+let input = document.getElementById('input'),
+    division = '/',
+    seven = '7',
+    eight = '8',
+    nine = '9',
+    multiply = '*',
+    four = '4',
+    five = '5',
+    six = '6',
+    minus = '-',
+    one = '1',
+    two = '2',
+    three = '3',
+    plus = '+',
+    nol = '0',
+    dot = '.',
+    kwadrat = '**',
+    arr = [];
+
+    function numberInInput(method) {
+        if(arr[arr.length-1] == '+' || arr[arr.length-1] == '-' || arr[arr.length-1] == '/' || arr[arr.length-1] == '*' || arr[arr.length-1] == '**' || arr[arr.length-1] == '.'){
+            if(method != '+' && method != '-' && method != '/' && method != '*' && method != '**' && method != '.') {
+                arr.push(method);
+                input.textContent = arr.join('');
+            }
+        }else {
+            arr.push(method);
+            input.textContent = arr.join('');
+        }
+    }
+
+    function noob() {
+        arr.pop();
+        input.textContent = arr.join('');
+        console.log(arr);
+    } 
+
+    function noobAll() {
+        arr = [];
+        input.textContent = arr.join('');
+    }
+
+    function liczenie() {
+        let licz = eval(arr.join(''));
+        input.textContent = licz;
+        arr = [];
+    }
